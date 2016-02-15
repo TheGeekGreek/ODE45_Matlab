84f213f353634f5e0a84c352cc2bc73d9477eb32
TheGeekGreek <baehni.yannis@gmx.ch>
2016-02-15 16:04:11 +0100

    Update direction_field.m

--- direction_field.m
+++ direction_field.m
@@ -1,7 +1,13 @@
-x = -1:.1:1;
+x = 0:1:10;
 y = x;
-[X, Y] = meshgrid(x, y);
-lambda = .5;
-V = -lambda * Y;
+[X, Y] = meshgrid(x, y)
+V = f(t, Y);
 U = ones(size(X));
 quiver(X, Y, U, V);
+hold on;
+tstart = 0;
+tend = 10;
+N = 10;
+y0 = 1;
+[t, y] = explicit_euler(@f, y0, tstart, tend, N);
+plot(t, y, 'r');
