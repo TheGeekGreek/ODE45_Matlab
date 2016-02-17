2a0cd729e7cfe1dc860853e691b50acc2dc5e125
Simon <gruenings@gmail.com>
2016-02-17 10:02:40 +0100

    more polish

--- explicit_euler.m
+++ explicit_euler.m
@@ -1,7 +1,17 @@
-function [ t, y ] = explicit_euler( f, y0, tstart, tend, steps )
-    h = (tend - tstart)/steps;
-    t = tstart:h:tend;
+% explicit_euler:
+%       Solves a differential using the Explicit Euler
+%
+% parameters: ( function , y0, timespan, bound)
+%   function -> f(t,y)
+%   timespan -> [start_time, end_time]
+%   y0       -> starting value
+%   steps    -> the number of steps
+
+function [ t, y ] = explicit_euler( f, y0, timespan, steps )
+    h = (timespan(2) - timespan(1))/steps;
+    t = timespan(1):h:timespan(2);
     y = [y0];
+    
     for k = 1:steps
         y(k + 1) = y(k) + h * f(t, y(k));
     end    
