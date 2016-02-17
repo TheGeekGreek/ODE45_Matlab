30423d02e8b1e9709ae78ca06b625f7122f97b16
TheGeekGreek <baehni.yannis@gmx.ch>
2016-02-17 09:21:02 +0100

    Update direction_field.m

--- direction_field.m
+++ direction_field.m
@@ -1,13 +1,20 @@
-x = 0:1:10;
-y = x;
-[X, Y] = meshgrid(x, y)
-V = f(t, Y);
-U = ones(size(X));
-quiver(X, Y, U, V);
-hold on;
-tstart = 0;
-tend = 10;
-N = 10;
-y0 = 1;
-[t, y] = explicit_euler(@f, y0, tstart, tend, N);
-plot(t, y, 'r');
+function [] = direction_field( func, y0, tstart, tend, bound )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    delta = .1 * bound;
+    x = -bound:delta:bound;
+    [X, Y] = meshgrid(x, x);
+    V = func(X, Y);
+    U = ones(size(X));
+    normed = 1./sqrt(V.^2 + U.^2);
+    quiver(X, Y, normed .* U, normed .* V, 'color', [0, 0, 0], 'ShowArrowHead', 'off', 'Marker', 'o');
+    hold on;
+    [t, y] = ODE45(func, tstart, tend, y0);   
+    plot(t, y, 'color', 'r', 'Marker', 'o');
+    xlim([-bound, bound]);
+    ylim([-bound, bound]);
+    ax = gca;
+    ax.XAxisLocation = 'bottom';
+    ax.YAxisLocation = 'left';
+    ax.Box = 'off';
+end
