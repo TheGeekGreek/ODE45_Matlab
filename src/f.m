2e94ed51e537dbbf79716884d65618516e1efbc3
TheGeekGreek <baehni.yannis@gmx.ch>
2016-02-17 11:24:43 +0100

    Update f.m

--- src/f.m
+++ src/f.m
@@ -1,8 +1,9 @@
 % Sample Functions
 
 function [ out ] = f( t, y )
-    out = -.5 * y;
-    %out = y.^2 - t;
+    %out = -.5 * y;
+    out = y.^2 - t;
+    %Function for ODE45 only, cannot be used for direction field
     %out = zeros(length(y),1);
     %out(1) = y(2);
     %out(2) = -sin(y(1));
